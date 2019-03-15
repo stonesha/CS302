f0d40c9b3ceba0fd158ced25841704ab693ae037
stonesha <stonesha@users.noreply.github.com>
2019-03-14 22:58:12 -0700

    Setup Header Files

---
+++ Homework 4/hw4/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include "BinaryTreeInterface.h"
+#include "BinaryNode.h"
+#include "BinaryNodeTree.h"
+#include "NotFoundException.h"
+#include "PrecondViolatedExcept.h"
+#include <memory>
+#include "BinarySearchTree.h"
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
