365a3675482bdba78799d7fc839112ccb47f8167
stonesha <stonesha@users.noreply.github.com>
2019-03-27 18:24:38 -0700

    Fleshed out push and pop for Heap

--- CS 302/Homework 5/hw5/main.cpp
+++ CS 302/Homework 5/hw5/main.cpp
@@ -1,43 +1,66 @@
+/**
+ * @file main.cpp
+ * @author Stone Sha (stones@nevada.unr.edu)
+ * @date March, 2019
+ * @brief Test program for Heap ADT using STL
+ */
 #include <iostream>
-#include <algorithm> // std::shuffle, std::
-#include <vector>
-#include <numeric>      // std::accumulate
-#include <random>
+#include <algorithm> //  std::make_heap,  std::pop_heap,  std::push_heap,  std::sort_heap
+#include <vector> // std::vector
+#include <numeric>  // std::accumulate
+#include <random> //std::random_device, std::mt199937, std::uniform_int_distribution
 
-void randGen(std::vector<int> &vec);
-void printVec(std::vector<int> vec);
+//using namespace for convenience sake since this program is small
+using namespace std;
+
+//prototyping for functions that help
+void randGen( vector<int> &vec);
+void printVec( vector<int> vec);
 
 int main()
 {
 
-    std::vector<int> v;
+    //vector initialized
+    vector<int> v;
 
+    //random values in vector
     randGen(v);
 
-    std::cout << std::endl << "=====Randomly Generated Vector=====" << std::endl;
+    cout <<  endl << "=====Randomly Generated Vector=====";
 
     printVec(v);
 
+    //makes vector a heap
     make_heap(v.begin(), v.end());
 
-    auto mean = std::accumulate(v.begin(), v.end(), 0) / v.size();
+    //checks if it's actually a heap
+    if(is_heap(v.begin(), v.end()))
+        cout <<  endl << "=====Vector is now a Heap=====";
 
+    //calculates mean using std::accumulate
+    auto mean =  accumulate(v.begin(), v.end(), 0) / v.size();
+
+    //pushes mean into heap
     v.push_back(mean);
-
-    std::cout << std::endl  << "=====Mean pushed onto Heap=====" << std::endl;
-
-    printVec(v);
-
-    pop_heap(v.begin(), v.end());
-    v.pop_back();
-
-    std::cout << std::endl  << "=====Maximum Deleted Element=====" << std::endl;
-
-    printVec(v);
-
     push_heap(v.begin(), v.end());
 
-    std::cout << std::endl  << "=====Sorted Heap=====" << std::endl;
+    cout <<  endl  << "=====Mean pushed onto Heap=====" << endl;
+    cout << "Mean is " << mean;
+    printVec(v);
+
+    //moves maximum element to back of vector
+    pop_heap(v.begin(), v.end());
+    //removes maximum element at the back
+    v.pop_back();
+
+    cout <<  endl  << "=====Maximum Deleted Element=====";
+
+    printVec(v);
+
+    //sorts heap in ascending order
+    sort_heap(v.begin(), v.end());
+
+    cout <<  endl  << "=====Sorted Heap=====";
 
     printVec(v);
 
@@ -46,31 +69,35 @@ int main()
 
 
 /**
-* Fills binary search tree with
+* Fills vector with
 * random values between 0 and 200
-* @param        BinarySearchTree object
+* @param        vector<int> vec
 **/
-void randGen(std::vector<int> &vec)
+void randGen( vector<int> &vec)
 {
     //initialization for random ints
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 999);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, 999);
 
     //fills vector with random values
     for(int i = 0; i < 100; i++)
         vec.push_back(dis(gen));
 }
 
-void printVec(std::vector<int> vec)
+/**
+* Prints values of vector
+* @param        vector<int> vec
+**/
+void printVec( vector<int> vec)
 {
-    std::cout << std::endl;
+    cout <<  endl;
 
     for(unsigned int i = 0; i < vec.size(); i++)
     {
         if(i % 10 == 0)
-            std::cout << std::endl;
+            cout <<  endl;
 
-        std::cout  << vec[i] << ", ";
+        cout  << vec[i] << ", ";
     }
 }
