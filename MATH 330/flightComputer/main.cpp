d11e382299c82da80f97c9e1363482a8e1766f65
stonesha <stonesha@users.noreply.github.com>
2019-04-09 22:42:35 -0700

    little changes

--- MATH 330/flightComputer/main.cpp
+++ MATH 330/flightComputer/main.cpp
@@ -1,44 +1,16 @@
 #include <iostream>
+#include "flightCompute.h"
 
-/**
-* Computes magnetic heading (degrees)
-* @param        theta = true heading in degrees
-* @param        v = indicated air speed (IAS) in knots
-* @param        h = height in feet
-* @param        w = wind speed in knots
-* @param        phi = wind direction in degrees
-* @param        t = temperature of air in Celsius
-**/
-void computeFlight(double theta, double v, double h,
-                   double w, double phi, double t);
-
-/**
-* Computes true ground speed (knots)
-* @param        theta = true heading in degrees
-* @param        v = indicated air speed (IAS) in knots
-* @param        h = height in feet
-* @param        w = wind speed in knots
-* @param        phi = wind direction in degrees
-* @param        t = temperature of air in Celsius
-**/
-void computeGroundSpeed(double theta, double v, double h,
-                   double w, double phi, double t);
+using namespace std;
 
 int main()
 {
+    double v = 100, h = 5000;
 
+    feetToMeters(h);
+
+    cout << "h: " << h << endl;
 
     return 0;
 }
 
-void computeFlight(double theta, double v, double h,
-                   double w, double phi, double t)
-{
-
-}
-
-void computeGroundSpeed(double theta, double v, double h,
-                   double w, double phi, double t)
-{
-
-}
