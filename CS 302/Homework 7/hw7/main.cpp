e4126404c951f5de79001e8b472c5121f607cf83
stonesha <stonesha@users.noreply.github.com>
2019-05-02 20:12:46 -0700

    moved everything to main

--- CS 302/Homework 7/hw7/main.cpp
+++ CS 302/Homework 7/hw7/main.cpp
@@ -1,8 +1,22 @@
-#include "Graph.h"
+#include <iostream>
+
+#define VERTICES 5
 
 int main()
 {
-	std::cout << "test" << std::endl;
+	/*
+	Distances were obtained using Google Maps
+	1 = Reno (R), 2 = San Francisco (SF), 3 = Salt Lake City(SLC),
+	4 = Seattle(SE), 5 = Las Vegas(LV)
+*/
+	int weightedGraph[VERTICES][VERTICES] =
+	{	/*R   SF  SLC   SE   LV
+   R*/	{ 0, 218, 518, 705, 439 },
+		/*SF*/	{ 218, 0, 735, 807, 569 },
+		/*SLC*/	{ 518, 735, 0, 840, 421 },
+		/*SE*/	{ 705, 807, 840, 0, 1116 },
+		/*LV*/	{ 439, 569, 421, 1116, 0 },
+	};
 
 	return 0;
 }
