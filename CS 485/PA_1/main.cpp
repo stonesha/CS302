9bcbd537de3f660bafd4c4679d4be4bbf6543567
Stone Sha <stoneshaus@gmail.com>
2020-02-05 23:07:11 -0800

    implemented 1D gaussian smoothing for CS 485 PA_1

--- CS 485/PA_1/main.cpp
+++ CS 485/PA_1/main.cpp
@@ -1,11 +1,28 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
-#include <opencv4>
+#include <cmath>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-int main(){
+void normalize(Mat &img);
+double gauss1D(double x, double sigma);
 
-	
+int main(int argc, char* argv[]){
+
 	return 0;
+}
+
+void normalize(Mat &img)
+{
+	img.convertTo(img, CV_32F, 1.0 / 255.0, 0);
+}
+
+double gauss1D(double x, double sigma)
+{
+	double expVal = -1 * (pow(x, 2) / (2 * pow(sigma, 2)));
+    double divider = sqrt(2 * M_PI * pow(sigma, 2));
+	return (1 / divider) * exp(expVal);
 }
