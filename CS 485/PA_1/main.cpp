b0024cb2da996d65146ec2b2e66749d8d3fbb704
Stone Sha <stoneshaus@gmail.com>
2020-02-12 18:08:39 -0800

    finished PA_1 for CS 485

--- CS 485/PA_1/main.cpp
+++ CS 485/PA_1/main.cpp
@@ -20,6 +20,8 @@ int main(int argc, char* argv[]) {
 
 	//variables for 1D Gaussian
 	ifstream fp("Rect_128.txt");
+	Mat src = imread("lenna.pgm");
+	Mat dst;	
 
 	VecD rect128;
 	VecD sigma1;
@@ -31,6 +33,8 @@ int main(int argc, char* argv[]) {
 		rect128.push_back(stod(line));
 	}
 
+	//Problem 1	
+	//(a)1D Gaussian smoothing
 	//Mask size = 5 * sigma
 	GaussianBlur(rect128, sigma1, Size(5, 0), 1, 0);
 	GaussianBlur(rect128, sigma5, Size(25, 0), 5, 0);
@@ -41,9 +45,28 @@ int main(int argc, char* argv[]) {
 	vecToFile("sigma5.txt", sigma5);
 	vecToFile("sigma11.txt", sigma11);
 
+	//(b) 2D Gaussian 
+	GaussianBlur(src, dst, Size(5, 5), 1, 1);
+	imwrite("lenna_sigma1_2D.jpg", dst);
 
+	GaussianBlur(src, dst, Size(25, 25), 5, 5);
+	imwrite("lenna_sigma5_2D.jpg", dst);
 
+	GaussianBlur(src, dst, Size(55, 55), 11, 11);
+	imwrite("lenna_sigma11_2D.jpg", dst);
 
+	//(c) 1D Gaussian on 2D Image
+	GaussianBlur(src, dst, Size(5, 0), 1, 0);
+	imwrite("lenna_sigma1_1D.jpg", dst);
+
+	GaussianBlur(src, dst, Size(25, 0), 5, 0);
+	imwrite("lenna_sigma5_1D.jpg", dst);
+
+	GaussianBlur(src, dst, Size(55, 0), 11, 0);
+	imwrite("lenna_sigma11_1D.jpg", dst);
+
+	
+	fp.close();
 	return 0;
 }
 
